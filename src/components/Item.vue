c3f78ac302240a8a54595d69802a24b7c4033d15
Nikita Bubnov <nb@redstart.pro>
2022-03-28 15:42:48 +0200

    completion of creation

---
+++ src/components/Item.vue
@@ -0,0 +1,53 @@
+<template>
+  <li class="item">
+    <input class="todo" type="text" v-model="element.name" :readonly="isReadonly">
+    <button class="btn " v-on:click="edit">{{ textButton }}</button>
+    <div class="btn danger" @click="removeClick">Удалить</div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Object
+  },
+  data() {
+    return {
+      isReadonly: true,
+      textButton: 'Редактировать',
+      localValue: ''
+    }
+  },
+  computed: {
+    element: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+  methods: {
+    removeClick() {
+      this.$emit("clickDeliteButton")
+
+    },
+    edit() {
+      this.isReadonly = !this.isReadonly
+      if (this.isReadonly) {
+        this.textButton = 'Редактировать'
+      } else {
+        this.textButton = 'Сохранить'
+      }
+    },
+  },
+
+}
+</script>
+
+<style>
+.todo:read-only {
+  border: none;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="list">
+        <Item v-for="(note, index) in value"  v-model="notes[index]" :key="index" v-on:clickDeliteButton="filterFun(index)" v-on:clickEdit="clickEdit(index)"/>
+    </ul>
+</template>
+
+<script>
+import Item from './Item.vue'
+
+
+export default {
+    name: 'List',
+  components: {
+    Item
+  },
+  props: {
+    value:Array
+  },
+  methods: {
+        filterFun(indexNote) {
+          this.$emit('deleteNote', indexNote)
+        },
+    },
+  computed:{
+    notes: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+}
+</script>
